7da4bd4a8705f030ab410e4cef67bf135f28d601
Mike <77847034+mikesilantev@users.noreply.github.com>
2022-03-25 11:34:13 +0300

    About Project 1280px

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,14 @@ import './App.css'
 
 import Header from "../Header/Header";
 import Promo from "../Main/Promo/Promo";
+import AboutProject from "../Main/AboutProject/AboutProject";
 
 export default function App(){
   return (
     <>
-    <Header></Header>
-    <Promo></Promo>
+      <Header></Header>
+      <Promo></Promo>
+      <AboutProject></AboutProject>
     </>
   )
 }
---
+++ src/components/Main/AboutProject/AboutProject.js
@@ -0,0 +1,46 @@
+import React from "react";
+import './AboutProject.css';
+
+import SectionTitle from "../SectionTitle/SectionTitle";
+
+export default function AboutProject() {
+  return (
+    <section className="about-project">
+      <SectionTitle text="О проекте"/>
+
+      <div className="about-project__articles">
+
+        <article className="about-project__article">
+          <h3 className="about-project__subtitle">
+            Дипломный проект включал 5 этапов
+          </h3>
+          <p className="about-project__paragraph">
+            Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
+          </p>
+        </article>
+
+        <article className="about-project__article">
+          <h3 className="about-project__subtitle">
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <p className="about-project__paragraph">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
+          </p>
+        </article>
+
+
+      </div>
+
+
+
+
+      <div className="about-project__progress-bar">
+        <p className="about-project__bar-item about-project__bar-item_green">1 неделя</p>
+        <p className="about-project__bar-item about-project__bar-item_gray">4 недели</p>
+        <p className="about-project__bar-item">Back-end</p>
+        <p className="about-project__bar-item">Front-end</p>
+      </div>
+
+    </section>
+  )
+}
