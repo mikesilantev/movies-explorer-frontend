afa65c02dbc80a4896def05d48a879f50766d873
Mike <77847034+mikesilantev@users.noreply.github.com>
2022-04-18 16:21:05 +0300

    lvl3 -> lvl2

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,36 +1,108 @@
-import react from 'react';
+import React, { useState } from 'react';
 import { Form } from '../Form/Forms';
 import './Register.css';
 
 
-export function Register() {
+export function Register({handleSignup}) {
+
+  //Состояния полей
+  const [ email, setEmail] = useState('')
+  const [ name, setName] = useState('')
+  const [ password, setPassword] = useState('')
+
+  // Изменение состояния инпута Email
+  function handleChangeEmail(evt){
+    setEmail(evt.target.value);
+    console.log(email);
+  }
+
+  // Изменение состояния инпута name
+  function handleChangeName(evt){
+    setName(evt.target.value);
+    console.log(name);
+  }
+
+  // Изменение состояния инпута password
+  function handleChangePassword(evt){
+    setPassword(evt.target.value);
+    console.log(password);
+  }
+
+  // Отправка данных в форме
+  function handleSubmitForm(evt){
+    evt.preventDefault();
+    handleSignup({
+      email,
+      name,
+      password,
+    })
+  }
+
 
   return (
     <section className="register">
+
+    {/* Прочитать заполненые пользователя
+    Проверить валидатором
+    Отправить на апи */}
           <Form 
-    title='Добро пожаловать!'
-    buttonText='Зарегистрироваться'
-    signMessage='Уже зарегистрированы?'
-    signLinkMessage='Войти'
-    signLink='/signin'
-    >
+            title='Добро пожаловать!'
+            buttonText='Зарегистрироваться'
+            signMessage='Уже зарегистрированы?'
+            signLinkMessage='Войти'
+            signLink='/signin'
+            onSubmit={handleSubmitForm}
+            >
+
         <div className="form__label-wrap">
         <label className='form__label'>
           <span className='form__input-name'>Имя</span>
-          <input type="text" className='form__input form__input_type_name'
-          placeholder='Виталий'/>
+
+          {/* input 'name' */}
+          <input 
+            type="text" 
+            className='form__input form__input_type_name'
+            placeholder='Виталий'
+
+            onChange={handleChangeName}
+          />
+          {/* input 'name' */}
+
           <span className='form__input-error'>Что-то пошло не так...</span>
         </label>
+
+
         <label className='form__label'>
           <span className='form__input-name'>E-mail</span>
-          <input type="text" className='form__input form__input_type_name'
-          placeholder='pochta@yandex.ru'/>
+
+          {/* input 'email' */}
+          <input 
+            type="email" 
+            className='form__input form__input_type_name'
+            placeholder='pochta@yandex.ru'
+
+            onChange={handleChangeEmail}
+          />
+          {/* input 'email' */}
+
+        
           <span className='form__input-error'>Что-то пошло не так...</span>
         </label>
+
+
         <label className='form__label'>
           <span className='form__input-name'>Пароль</span>
-          <input type="text" className='form__input form__input_type_name'
-          placeholder='••••••••••••••'/>
+
+          {/* input 'password' */}
+          <input 
+            type="password" 
+            className='form__input form__input_type_name'
+            placeholder='••••••••••••••'
+
+            onChange={handleChangePassword}
+          />
+          {/* input 'password' */}
+
           <span className='form__input-error'>Что-то пошло не так...</span>
         </label>
 
