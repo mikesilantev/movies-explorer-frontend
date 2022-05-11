9f2e9e792f754978b881c1de769749ca53360b42
Mike <77847034+mikesilantev@users.noreply.github.com>
2022-05-11 14:26:39 +0300

    day 2: fail

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -49,7 +49,6 @@ export default function App() {
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
   useEffect(() => {
     function checkToken() {
-      console.log('Запускаю CHECKTOKEN')
       let token = localStorage.getItem('JWT_TOKEN');
       if (token) {
         mainApi.getUser(token)
@@ -62,11 +61,12 @@ export default function App() {
           )
       }
     }
+    checkToken();
   }, [])
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
   //~~~~~~~~~~~~~~Регистрация~~/signup~~~~~~~~~~~~~~//
   // Дописать ошибки
-          // написать функцию для ошибок
+  // написать функцию для ошибок
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
   function handleSignup(data) {
     mainApi.signup({ data })
@@ -98,7 +98,7 @@ export default function App() {
           setApiErrorText('Вы ввели неправильный логин или пароль.')
         }
         else if (err === 'Ошибка: 401') {
-          setApiErrorText('Слишком много запросов, пожалуйста, повторите попытку позже')
+          setApiErrorText('Вы ввели неправильный логин или пароль.')
         }
         else if (err === 'Ошибка: 409') {
           setApiErrorText('Слишком много запросов, пожалуйста, повторите попытку позже')
@@ -116,225 +116,218 @@ export default function App() {
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
-
-  // Авторизация пользователя /signin
-  // function handleSignin(data) {
-  //   mainApi.signin({ data })
-  //     //Получаем в ответ токен
-  //     .then((res) => {
-  //       localStorage.setItem('JWT_TOKEN', res.token);
-  //       setCurrentUser(data)
-  //       setLoggedIn(true)
-  //       navigate('/movies', { replace: true });
-  //     })
-  //     .catch(err => {
-  //       if (err === 'Ошибка: 400') {
-  //         setApiErrorText('Вы ввели неправильный логин или пароль.')
-  //       }
-  //       if (err === 'Ошибка: 401') {
-  //         setApiErrorText('Слишком много запросов, пожалуйста, повторите попытку позже')
-  //       }
-  //       if (err === 'Ошибка: 409') {
-  //         setApiErrorText('Слишком много запросов, пожалуйста, повторите попытку позже')
-  //       }
-  //       if (err === 'Ошибка: 429') {
-  //         setApiErrorText('Слишком много запросов, пожалуйста, повторите попытку позже')
-  //       }
-  //       else {
-  //         setApiErrorText('Вы ввели неправильный логин или пароль. ')
-  //         console.log(apiErrorText)
-  //       }
-
-  //     })
-  // }
-
-
-
-
-
-
-  // Загрузка в локал файла с фильмами
   useEffect(() => {
-        let moviesToLocalStorage = localStorage.getItem('initialMovies');
-
-        if (loggedIn && !moviesToLocalStorage) {
-          try {
-            console.log('first')
-            async function getMovies() {
-              const getMovies = await movieApi.getMovies();
-              moviesToLocalStorage = await localStorage.setItem('initialMovies', JSON.stringify(getMovies));
-              const getLocalMovies = await JSON.parse(localStorage.getItem('initialMovies'));
-              setInitialMovies(getLocalMovies);
-            }
-            getMovies();
-          }
-          catch (err) {
-            console.log(err);
-          }
-          finally {
-            console.log('Закрывай прелоадер');
-          }
-        } else {
-          console.log('НИЧЕГО НЕ ДЕЛАЕМ')
-          async function saveMoveToState() {
-            const getLocalMovies = await JSON.parse(localStorage.getItem('initialMovies'));
-            setInitialMovies(getLocalMovies);
-            // console.log(initialMovies)
-          }
-          saveMoveToState();
-        }
-      }, [loggedIn]);
-
-
-    // Поведение поля поиска при вводе 
-    useEffect(() => {
-      // console.log(initialMovies)
-      // console.log(searchQuery)
-      async function filterMovie() {
-        const perfomMovies = await initialMovies.filter(
-          movie => (
-            searchQuery ?
-              movie.nameRU.toLowerCase().includes(searchQuery.toLowerCase()) : true)
-            &&
-            (checkboxStatus ? (movie.duration <= 40) : (movie.duration >= 0))
-        )
-        // console.log(perfomMovies)
-        setFilteredMovies(perfomMovies);
-      }
-      filterMovie();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [searchQuery, searchResult, checkboxStatus]);
-
-
-    // Нажатие по кнопке поиска
-    // заносит данные в стейт с результатми поиска
-    // и передает инфо в функцию сохранения в локал
-    async function searchByQuery() {
-      console.log('кнопка поиска')
-      // console.log('searchByQuery')
-      // console.log(filteredMovies)
-      saveToLocaleStorage(filteredMovies)
-      setSearchResult(filteredMovies)
+    let moviesToLocalStorage = localStorage.getItem('initialMovies');
+    if (moviesToLocalStorage && loggedIn) {
+      moviesToState(setInitialMovies, moviesToLocalStorage);
+    } else if (loggedIn){
+      uploadInitMovie(moviesToLocalStorage);
     }
+  }, [loggedIn]);
 
 
-    // Сохраняем данные запроса
-    function saveToLocaleStorage(searсh) {
-      localStorage.setItem('filteredMovies', JSON.stringify(searсh))
-      localStorage.setItem('checkboxStatus', checkboxStatus)
-      localStorage.setItem('searchQuery', searchQuery)
+  function moviesToState(stateFunc, el) {
+    stateFunc(JSON.parse(el))
+  }
+
+  function uploadInitMovie(el) {
+    movieApi.getMovies()
+      .then((res) => {
+        el = localStorage.setItem('initialMovies', JSON.stringify(res));
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .then()
+  }
+
+
+
+
+
+
+  // // Загрузка в локал файла с фильмами
+  // useEffect(() => {
+  //       let moviesToLocalStorage = localStorage.getItem('initialMovies');
+
+  //       if (loggedIn && !moviesToLocalStorage) {
+  //         try {
+  //           console.log('first')
+  //           async function getMovies() {
+  //             const getMovies = await movieApi.getMovies();
+  //             moviesToLocalStorage = await localStorage.setItem('initialMovies', JSON.stringify(getMovies));
+  //             const getLocalMovies = await JSON.parse(localStorage.getItem('initialMovies'));
+  //             setInitialMovies(getLocalMovies);
+  //           }
+  //           getMovies();
+  //         }
+  //         catch (err) {
+  //           console.log(err);
+  //         }
+  //         finally {
+  //           console.log('Закрывай прелоадер');
+  //         }
+  //       } else {
+  //         console.log('НИЧЕГО НЕ ДЕЛАЕМ')
+  //         async function saveMoveToState() {
+  //           const getLocalMovies = await JSON.parse(localStorage.getItem('initialMovies'));
+  //           setInitialMovies(getLocalMovies);
+  //           // console.log(initialMovies)
+  //         }
+  //         saveMoveToState();
+  //       }
+  //     }, [loggedIn]);
+
+
+  // Поведение поля поиска при вводе 
+  useEffect(() => {
+    // console.log(initialMovies)
+    // console.log(searchQuery)
+    async function filterMovie() {
+      const perfomMovies = await initialMovies.filter(
+        movie => (
+          searchQuery ?
+            movie.nameRU.toLowerCase().includes(searchQuery.toLowerCase()) : true)
+          &&
+          (checkboxStatus ? (movie.duration <= 40) : (movie.duration >= 0))
+      )
+      // console.log(perfomMovies)
+      setFilteredMovies(perfomMovies);
     }
-
-    function saveMovieToDb(item) {
-      const token = localStorage.getItem('JWT_TOKEN');
-      mainApi.saveMovie(item, token)
-    }
-
-    function removeMovieFromDb(id) {
-      const token = localStorage.getItem('JWT_TOKEN');
-      mainApi.removeMovie(id, token)
-    }
+    filterMovie();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchQuery, searchResult, checkboxStatus]);
 
 
+  // Нажатие по кнопке поиска
+  // заносит данные в стейт с результатми поиска
+  // и передает инфо в функцию сохранения в локал
+  async function searchByQuery() {
+    console.log('кнопка поиска')
+    // console.log('searchByQuery')
+    // console.log(filteredMovies)
+    saveToLocaleStorage(filteredMovies)
+    setSearchResult(filteredMovies)
+  }
 
 
-    function patchUser(data) {
-      let token = localStorage.getItem('JWT_TOKEN');
-      mainApi.patchUser(data, token)
-        .then((res) => console.log(res))
-    }
+  // Сохраняем данные запроса
+  function saveToLocaleStorage(searсh) {
+    localStorage.setItem('filteredMovies', JSON.stringify(searсh))
+    localStorage.setItem('checkboxStatus', checkboxStatus)
+    localStorage.setItem('searchQuery', searchQuery)
+  }
+
+  function saveMovieToDb(item) {
+    const token = localStorage.getItem('JWT_TOKEN');
+    mainApi.saveMovie(item, token)
+  }
+
+  function removeMovieFromDb(id) {
+    const token = localStorage.getItem('JWT_TOKEN');
+    mainApi.removeMovie(id, token)
+  }
 
 
-    // logout
-    function handleLogout() {
-      setLoggedIn(false);
-      setSearchQuery('');
-      removeLocalStorageOnExit();
-      navigate('/', { replace: true });
-    }
 
-    function removeLocalStorageOnExit() {
-      localStorage.removeItem('JWT_TOKEN')
-      localStorage.removeItem('checkboxStatus')
-      localStorage.removeItem('initialMovies')
-      localStorage.removeItem('savedMovies')
-      localStorage.removeItem('searchQuery')
-      localStorage.removeItem('filteredMovies')
-    }
 
-    return (
+  function patchUser(data) {
+    let token = localStorage.getItem('JWT_TOKEN');
+    mainApi.patchUser(data, token)
+      .then((res) => console.log(res))
+  }
 
-      <CurrentUserContext.Provider value={currentUser}>
-        <Routes>
-          <Route path='/' element={
-            <AppLayout
 
+  // logout
+  function handleLogout() {
+    setLoggedIn(false);
+    setSearchQuery('');
+    removeLocalStorageOnExit();
+    navigate('/', { replace: true });
+  }
+
+  function removeLocalStorageOnExit() {
+    localStorage.removeItem('JWT_TOKEN')
+    localStorage.removeItem('checkboxStatus')
+    localStorage.removeItem('initialMovies')
+    localStorage.removeItem('savedMovies')
+    localStorage.removeItem('searchQuery')
+    localStorage.removeItem('filteredMovies')
+  }
+
+  return (
+
+    <CurrentUserContext.Provider value={currentUser}>
+      <Routes>
+        <Route path='/' element={
+          <AppLayout
+
+            loggedIn={loggedIn}
+
+          />}>
+          <Route index element={<Main />} />
+          <Route element={<ProtectedRoute loggedIn={loggedIn} />}>
+            <Route path='movies' element={
+              <Movies
+                searchQuery={searchQuery}
+                setSearchQuery={setSearchQuery}
+                checkboxStatus={checkboxStatus}
+                setCheckboxStatus={setCheckboxStatus}
+                searchByQuery={searchByQuery}
+                initialMovies={initialMovies}
+                searchResult={searchResult}
+                filteredMovies={filteredMovies}
+                saveMovieToDb={saveMovieToDb}
+                removeMovieFromDb={removeMovieFromDb}
+              />
+            }></Route>
+            <Route path='saved-movies' element={
+              <SavedMovies
+                searchQuery={searchQuery}
+                setSearchQuery={setSearchQuery}
+                checkboxStatus={checkboxStatus}
+                setCheckboxStatus={setCheckboxStatus}
+                searchByQuery={searchByQuery}
+                initialMovies={initialMovies}
+                searchResult={searchResult}
+                filteredMovies={filteredMovies}
+                saveMovieToDb={saveMovieToDb}
+                removeMovieFromDb={removeMovieFromDb}
+              />}></Route>
+          </Route>
+        </Route>
+
+        <Route element={<ProtectedRoute loggedIn={loggedIn} />}>
+          <Route path='/profile' element=
+            {<ProfileLayout
               loggedIn={loggedIn}
 
             />}>
-            <Route index element={<Main />} />
-            <Route element={<ProtectedRoute loggedIn={loggedIn} />}>
-              <Route path='movies' element={
-                <Movies
-                  searchQuery={searchQuery}
-                  setSearchQuery={setSearchQuery}
-                  checkboxStatus={checkboxStatus}
-                  setCheckboxStatus={setCheckboxStatus}
-                  searchByQuery={searchByQuery}
-                  initialMovies={initialMovies}
-                  searchResult={searchResult}
-                  filteredMovies={filteredMovies}
-                  saveMovieToDb={saveMovieToDb}
-                  removeMovieFromDb={removeMovieFromDb}
-                />
-              }></Route>
-              <Route path='saved-movies' element={
-                <SavedMovies
-                  searchQuery={searchQuery}
-                  setSearchQuery={setSearchQuery}
-                  checkboxStatus={checkboxStatus}
-                  setCheckboxStatus={setCheckboxStatus}
-                  searchByQuery={searchByQuery}
-                  initialMovies={initialMovies}
-                  searchResult={searchResult}
-                  filteredMovies={filteredMovies}
-                  saveMovieToDb={saveMovieToDb}
-                  removeMovieFromDb={removeMovieFromDb}
-                />}></Route>
-            </Route>
+            <Route index element=
+              {<Profile
+                patchUser={patchUser}
+                apiErrorText={apiErrorText}
+                handleLogout={handleLogout}
+              />} />
           </Route>
-
-          <Route element={<ProtectedRoute loggedIn={loggedIn} />}>
-            <Route path='/profile' element=
-              {<ProfileLayout
-                loggedIn={loggedIn}
-
-              />}>
-              <Route index element=
-                {<Profile
-                  patchUser={patchUser}
-                  apiErrorText={apiErrorText}
-                  handleLogout={handleLogout}
-                />} />
-            </Route>
-          </Route>
+        </Route>
 
 
-          <Route path='/signup'
-            element=
-            {<Register
-              handleSignup={handleSignup}
-            />} />
+        <Route path='/signup'
+          element=
+          {<Register
+            handleSignup={handleSignup}
+          />} />
 
-          <Route path='/signin'
-            element=
-            {<Login
-              handleSignin={handleSignin}
-              apiErrorText={apiErrorText}
-            />}
-          />
-          <Route path='*' element={<PageNotFound />} />
-        </Routes>
-      </CurrentUserContext.Provider>
-    )
-  }
+        <Route path='/signin'
+          element=
+          {<Login
+            handleSignin={handleSignin}
+            apiErrorText={apiErrorText}
+          />}
+        />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </CurrentUserContext.Provider>
+  )
+}
